7437e404519e38b99fb11ee554b5616e4f6ddadb
Ts. Abd Rahim Kasiman <holy_rainman@yahoo.com>
2025-10-05 22:09:56 +0800

    Create Blynk_Basic.ino

---
+++ Blynk_Basic.ino
@@ -0,0 +1,20 @@
+#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID       "TMPxxxxxx"
+#define BLYNK_TEMPLATE_NAME     "Device"
+#define BLYNK_AUTH_TOKEN        "YourAuthToken"
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
+
+void setup()
+{ Serial.begin(9600);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+}
+
+void loop()
+{ Blynk.run();
+}
