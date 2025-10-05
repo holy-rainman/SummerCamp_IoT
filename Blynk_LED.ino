773345f0a7126f7726ba1673bf7ca74e7db72cc1
Ts. Abd Rahim Kasiman <holy_rainman@yahoo.com>
2025-10-05 23:17:36 +0800

    Create Blynk_LED.ino

---
+++ Blynk_LED.ino
@@ -0,0 +1,26 @@
+#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID       "TMPxxxxxx"
+#define BLYNK_TEMPLATE_NAME     "Device"
+#define BLYNK_AUTH_TOKEN        "YourAuthToken"
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
+
+#define LED 23
+BLYNK_WRITE(V0)
+{ int pbBlynk = param.asInt();
+  digitalWrite(LED,pbBlynk);
+}
+
+void setup()
+{ Serial.begin(9600);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  pinMode(LED,OUTPUT);
+}
+
+void loop()
+{ Blynk.run();
+}
